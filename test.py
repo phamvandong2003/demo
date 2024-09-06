3fa5429b05fded271b2f5eb528ec0a214a89f251
Pham Van Dong <dong2k3sv6@gmail.com>
2024-09-06 09:16:45 +0700

    Them file python

--- test.py
+++ test.py
@@ -5,3 +5,12 @@ b = 8
 
 tong = a + b
 print(tong)
+
+def tinhtich():
+    a = float(input("Nhap gia tri a = "))
+    
+    b = float(input("Nhap gia tri b= "))
+    
+    tich = a*b
+    print("Tich hai so = ",tich)
+tinhtich()
