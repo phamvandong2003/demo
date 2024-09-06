d01bea90ca1ba39d6205a116a094026c1847137a
Pham Van Dong <dong2k3sv6@gmail.com>
2024-09-06 08:39:40 +0700

    Them cpp

--- Test.cpp
+++ Test.cpp
@@ -4,4 +4,12 @@ using namespace std;
 int main(){
     cout<<"Hello word";
     cout<<"Tao la bo may";
+
+    int a,b, tong;
+    a = 10;
+    cout<<"Nhap b";
+    cin>>b;
+    tong = a+b;
+    cout<<"Tong hai so = ",tong;
+    
 }
